dc2c313281c6a0882d0438c0ab5a0d34e2f44871
sam <samgathon@hotmail.com>
2019-09-22 01:56:30 -0500

    adding animals

--- animalcollector/settings.py
+++ animalcollector/settings.py
@@ -76,11 +76,10 @@ WSGI_APPLICATION = 'animalcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'animalcollector',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+class Animal:  
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+animals = [
+  Animal('Lion', 'mufasa', 'little demon', 3),
+  Animal('Tiger', 'brown', 'crazy', 0),
+  Animal('Hyena', 'black tripod', '3 legged hyena', 4)
+]    
+
+def home(request):
+  return HttpResponse('<h1>About The Animal collector</h1>')
+
+def about(request):
+  return render(request, 'about.html')  
+def animals_index(request):
+  return render(request, 'animals/index.html', { 'animals': animals })  
